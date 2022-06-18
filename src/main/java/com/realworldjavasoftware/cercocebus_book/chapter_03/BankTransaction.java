60d138e71226c7162beef5ea8df965bb1fb009e3
doxxx <doxxx93@gmail.com>
2022-06-18 10:53:34 +0900

    chore: build for chapter 3

---
+++ src/main/java/com/realworldjavasoftware/cercocebus_book/chapter_03/BankTransaction.java
@@ -0,0 +1,56 @@
+package com.realworldjavasoftware.cercocebus_book.chapter_03;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public final class BankTransaction {
+
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+    public BankTransaction(LocalDate date, double amount, String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" +
+            "date=" + date +
+            ", amount=" + amount +
+            ", description='" + description + '\'' +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BankTransaction that = (BankTransaction) o;
+        return Double.compare(that.amount, amount) == 0 && Objects.equals(date,
+            that.date) && Objects.equals(description, that.description);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, amount, description);
+    }
+}
