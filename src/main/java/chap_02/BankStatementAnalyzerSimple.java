c6eb4a32db224b6045dc7fd8305aaab67776bafb
doxxx <doxxx93@gmail.com>
2023-03-09 12:21:51 +0900

    fix typo

--- src/main/java/chap_02/BankStatementAnalyzerSimple.java
+++ src/main/java/chap_02/BankStatementAnalyzerSimple.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class BankTransactionAnalyzerSimple {
+public class BankStatementAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void main(String[] args) throws IOException {
